a10108501ac5002a2005a4dd4839c89bf95b217e
Loubna <megloudloubna@gmail.com>
2024-09-15 23:39:19 +0200

    react

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,12 @@
+import './App.css'
+import UserProfile from './components/UserProfile'
+
 function App() {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Welcome to the Recipe Sharing Platform
-      </h1>
-    </div>
-  );
+    <>
+      <UserProfile />
+    </>
+  )
 }
 
-export default App;
+export default App
