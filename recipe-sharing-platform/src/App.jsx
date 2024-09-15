aae8988851fbd75c7384273744ee948cb06ab4fb
Loubna <megloudloubna@gmail.com>
2024-09-15 22:11:15 +0200

    react platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,20 @@
+import React from 'react'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail'
+import AddRecipeForm from './components/AddRecipeForm'
+
 function App() {
-    return (
-      <div className="text-center p-10">
-        <h1 className="text-4xl font-bold text-blue-500">
-          Welcome to the Recipe Sharing Platform
-        </h1>
-      </div>
-    );
-  }
-  
-  export default App;
-  
+
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+export default {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
