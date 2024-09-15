f551a5ffb58457cd51bb15ccfd194ddcbdbeae29
Loubna <megloudloubna@gmail.com>
2024-09-15 21:39:13 +0200

    platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-4xl font-bold text-blue-500">
+          Welcome to the Recipe Sharing Platform
+        </h1>
+      </div>
+    );
+  }
+  
+  export default App;
+  
